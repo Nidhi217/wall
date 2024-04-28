b914c3cf9f03eb3ae5dc4280b2d5e1daf60eada3
schub <schub@NITRO>
2024-04-28 18:04:42 +0530

    sss

---
+++ beef/src/beef/vein.java
@@ -0,0 +1,14 @@
+package beef;
+
+public class vein {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("yell");
+		
+	}
+
+}
---
+++ beef/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author schub
+ *
+ */
+module beef {
+}
